4fcee6c7cfad52c8c6b1ba8e27d7cf703081aca4
Minh Truong <mptruong49@gmail.com>
2023-03-19 22:54:55 -0600

    Day 4, incomplete

---
+++ day_4/strings.py
@@ -0,0 +1,16 @@
+# 30DaysOfPython Day 4
+
+print("Thirty" + "Days" + "Of" + "Python")
+
+company = "Coding For All"
+print(company)
+print(len(company))
+print(company.upper())
+print(company.lower())
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+print(company.swapcase())
+print(company[0:1])
+
+
